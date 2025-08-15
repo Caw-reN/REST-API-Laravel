6e6a84f699cb2afd49571ed25cb00ea20e959907
Caw-reN <hibrizitsaqif30@gmail.com>
2025-08-15 15:26:29 +0700

    initial commit

---
+++ app/Http/Controllers/Api/BukuController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Buku;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class BukuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Buku::orderBy('id', 'asc')->get();
+        return response()->json([
+            'status' => true,
+            'message' => 'Data ditemukan',
+            'data' => $data
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $dataBuku = new Buku;
+
+        $rules = [
+            'judul' => 'required',
+            'pengarang' => 'required',
+            'tanggal_publikasi' => 'required|date'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data gagal ditambahkan',
+                'data' => $validator->errors()
+            ]);
+        }
+
+        $dataBuku->judul = $request->judul;
+        $dataBuku->pengarang = $request->pengarang;
+        $dataBuku->tanggal_publikasi = $request->tanggal_publikasi;
+
+        $dataBuku->save();
+        return response([
+            'status' => true,
+            'message' => 'Data berhasil ditambahkan'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+        $data = Buku::find($id);
+
+        if($data) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Data Ditemukan',
+                'data' => $data
+            ],200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan'
+            ]);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $dataBuku = Buku::find($id);
+
+        if (empty($dataBuku)) {
+            return response([
+                'status' => false,
+                'message' => 'Data tidak ditemukan',
+            ], 404);
+        }
+
+        $rules = [
+            'judul' => 'required',
+            'pengarang' => 'required',
+            'tanggal_publikasi' => 'required|date'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Gagal melakukan update data',
+                'data' => $validator->errors()
+            ]);
+        }
+
+        $dataBuku->judul = $request->judul;
+        $dataBuku->pengarang = $request->pengarang;
+        $dataBuku->tanggal_publikasi = $request->tanggal_publikasi;
+
+        $dataBuku->save();
+
+        return response([
+            'status' => true,
+            'message' => 'Berhasil melakukan update data'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $dataBuku = Buku::find($id);
+
+        if (empty($dataBuku)) {
+            return response([
+                'status' => false,
+                'message' => 'Data tidak ditemukan',
+            ], 404);
+        }
+
+        $dataBuku->delete();
+
+        return response([
+            'status' => true,
+            'message' => 'Berhasil menghapus data'
+        ]);
+    }
+}
