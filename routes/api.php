4f36a9a0088fd2d0869631bef98bed95c2ce8a3e
Caw-reN <hibrizitsaqif30@gmail.com>
2025-08-24 22:44:43 +0700

    Add host api access permissions

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\BukuController;
+use App\Http\Middleware\CekHost;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,8 +9,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('buku', [BukuController::class, 'index']);
-Route::get('buku/{id}', [BukuController::class, 'show']);
-Route::post('buku', [BukuController::class, 'store']);
-Route::put('buku/{id}', [BukuController::class, 'update']);
-Route::delete('buku/{id}', [BukuController::class, 'destroy']);
+Route::get('buku', [BukuController::class, 'index'])->middleware(CekHost::class);
+Route::get('buku/{id}', [BukuController::class, 'show'])->middleware(CekHost::class);
+Route::post('buku', [BukuController::class, 'store'])->middleware(CekHost::class);
+Route::put('buku/{id}', [BukuController::class, 'update'])->middleware(CekHost::class);
+Route::delete('buku/{id}', [BukuController::class, 'destroy'])->middleware(CekHost::class);
+
+// Route::apiResource('buku', [BukuController::class]);
